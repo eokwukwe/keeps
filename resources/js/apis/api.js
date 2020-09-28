d7467c897958472f50de65f83e08dfe583720fb5
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2020-09-28 08:54:42 +0100

    feat(deploy): prepare for deployment

--- resources/js/apis/api.js
+++ resources/js/apis/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+console.log("api url", process.env.NODE_ENV);
+
 let baseApi = axios.create({
-    baseURL: "http://keeps.test/api"
+    baseURL:
+        process.env.NODE_ENV === "development"
+            ? "http://keeps.test/api"
+            : "https://fcode-larakeeps.herokuapp.com/api"
 });
 
 let api = function() {
